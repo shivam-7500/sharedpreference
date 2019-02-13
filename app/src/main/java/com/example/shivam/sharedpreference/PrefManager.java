a9342b5091f6b3b574b19eb0866a553f5eca4566
shivam-7500 <shivamaggarwal2911@gmail.com>
2019-02-13 10:54:15 +0530

    First

---
+++ app/src/main/java/com/example/shivam/sharedpreference/PrefManager.java
@@ -0,0 +1,14 @@
+package com.example.shivam.sharedpreference;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class PrefManager {
+    Context context;
+
+public void saveLogInDetails(String email,String password)
+{
+
+}
+
+}
